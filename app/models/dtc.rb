502d4ae586bdfc500663d28166e3581164839358
Sébastien Pech <sebastien.pech@gmail.com>
2023-04-02 17:05:51 +0200

    creation with webpage

--- app/models/dtc.rb
+++ app/models/dtc.rb
@@ -1,6 +1,5 @@
 class Dtc < ApplicationRecord
-    validates :shortText, :pCode, :spnFmi, presence: { message: "Cannot create %{model} without %{attribute}" }, presence: true, uniqueness: true
-    validates :pCode, format: { with: /[UPC][0-9A-F]{4}-[0-9A-F]{2}/ , message: "Invalid %{attribute} format"}, presence: true, uniqueness: true
-    validates :spnFmi, format: { with: /\d+\/\d+/ , message: "Invalid %{attribute} format"}, presence: true, uniqueness: true
-    
+    validates :shortText, :pCode, :spnFmi, presence: { message: "Cannot create %{model} without %{attribute}" }, uniqueness: true
+    validates :pCode, format: { with: /[UPB][0-9A-F]{4}-[0-9A-F]{2}/ , message: "Invalid %{attribute} format"}
+    validates :spnFmi, format: { with: /\d+\/\d+/ , message: "Invalid %{attribute} format"}
 end

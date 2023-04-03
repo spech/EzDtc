8d32a2633f051e7ee22a93cfa95553d3a27dcc72
Sébastien Pech <sebastien.pech@gmail.com>
2023-04-03 20:34:08 +0200

    adding complex validation for  codes

--- app/models/dtc.rb
+++ app/models/dtc.rb
@@ -1,5 +1,34 @@
 class Dtc < ApplicationRecord
     validates :shortText, :pCode, :spnFmi, presence: { message: "Cannot create %{model} without %{attribute}" }, uniqueness: true
     validates :pCode, format: { with: /[UPB][0-9A-F]{4}-[0-9A-F]{2}/ , message: "Invalid %{attribute} format"}
-    validates :spnFmi, format: { with: /\d+\/\d+/ , message: "Invalid %{attribute} format"}
+    validate :spn_fmi_format_is_valid
+    validate :p_code_format_is_valid
+
+    def spn_fmi_format_is_valid
+        capture = spnFmi.match(/(?<spn>\d+)\/(?<fmi>\d+)/)
+        if capture 
+            unless capture[:spn].to_i < (2**19 -1)
+                errors.add(:spnFmi, "SPN cannot exceed 2**19 - 1")
+            end
+            unless capture[:fmi].to_i < 31
+                errors.add(:spnFmi, "FMI cannot exceed 31")
+            end
+        else
+            errors.add(:spnFmi, "Invalid SPN/FMI format")
+        end
+    end
+
+    def p_code_format_is_valid
+        capture = pCode.match(/[UPB](?<high>[0-9A-F]{4})-(?<low>[0-9A-F]{2})/ )
+        if capture 
+            unless capture[:high].to_i < 65535
+                errors.add(:pCode, "UDS High cannot exceed 65535")
+            end
+            unless capture[:low].to_i < 255
+                errors.add(:pCode, "UDS Low cannot exceed 255")
+            end
+        else
+            errors.add(:pCode, "Invalid PCode format")
+        end
+    end
 end

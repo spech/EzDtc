cef0de21bd4f71e853035c7b8f4c5bca49e3bbb8
Sébastien Pech <sebastien.pech@gmail.com>
2023-04-04 20:22:14 +0200

    validate and rspec model

--- app/models/dtc.rb
+++ app/models/dtc.rb
@@ -1,15 +1,16 @@
 class Dtc < ApplicationRecord
     validates :shortText, :pCode, :spnFmi, presence: { message: "Cannot create %{model} without %{attribute}" }, uniqueness: true
+validates :shortText, format: { with: /\w{2,}/, message: "ShortText is too short"}
     validate :spn_fmi_format_is_valid
     validate :p_code_format_is_valid
 
     def spn_fmi_format_is_valid
         capture = spnFmi.match(/(?<spn>\d+)\/(?<fmi>\d+)/)
         if capture 
-            unless capture[:spn].to_i < (2**19 -1)
+            unless capture[:spn].to_i < 0x7FFFF
                 errors.add(:spnFmi, "SPN cannot exceed 2**19 - 1")
             end
-            unless capture[:fmi].to_i < 31
+            unless capture[:fmi].to_i < 0x1F
                 errors.add(:spnFmi, "FMI cannot exceed 31")
             end
         else
@@ -20,11 +21,11 @@ class Dtc < ApplicationRecord
     def p_code_format_is_valid
         capture = pCode.match(/[UPB](?<high>[0-9A-F]{4})-(?<low>[0-9A-F]{2})/ )
         if capture 
-            unless capture[:high].to_i < 65535
-                errors.add(:pCode, "UDS High cannot exceed 65535")
+            unless capture[:high].to_i(base=16) < 0x1FFF
+                errors.add(:pCode, "UDS High cannot exceed 0x1FFF")
             end
-            unless capture[:low].to_i < 255
-                errors.add(:pCode, "UDS Low cannot exceed 255")
+            unless capture[:low].to_i(base=16) < 0x3F
+                errors.add(:pCode, "UDS Low cannot exceed 0x2F")
             end
         else
             errors.add(:pCode, "Invalid PCode format")
--- spec/models/dtc_spec.rb
+++ spec/models/dtc_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Dtc, type: :model do
   it "is valid with valid attributes" do
-    dtc = Dtc.new(shortText: "Anything you want is ok",pCode: "P1234-AB", spnFmi: "1234/3")
+    dtc = Dtc.new(shortText: "Anything you want is ok",pCode: "P1234-1A", spnFmi: "1234/3")
     expect(dtc).to be_valid
   end
 
@@ -13,10 +13,29 @@ RSpec.describe Dtc, type: :model do
   end
 
 
-  it "is not valid format if SPN is too big"
-  it "is not valid format if FMI is too big"
-  it "is not valid format if PCode High is too big"
-  it "is not valid format if PCode Low is too big"
-  it "is not valid format if ShortText is not a sentence"
+  it "is not valid format if SPN is too big" do
+    dtc = Dtc.new(shortText: "Anything you want is ok",pCode: "P1234-1A", spnFmi: "1234567899/3")
+    expect(dtc).not_to be_valid
+  end
+
+  it "is not valid format if FMI is too big" do
+    dtc = Dtc.new(shortText: "Anything you want is ok",pCode: "P1234-1A", spnFmi: "1234/33")
+    expect(dtc).not_to be_valid
+  end
+
+  it "is not valid format if PCode High is too big" do
+    dtc = Dtc.new(shortText: "Anything you want is ok",pCode: "P12345-1A", spnFmi: "1234/3")
+    expect(dtc).not_to be_valid
+  end
+
+  it "is not valid format if PCode Low is too big" do
+    dtc = Dtc.new(shortText: "Anything you want is ok",pCode: "P1234-4A", spnFmi: "1234/3")
+    expect(dtc).not_to be_valid
+  end
+
+  it "is not valid format if ShortText is not a sentence" do
+    dtc = Dtc.new(shortText: "AAnything" ,pCode: "P1234-1A", spnFmi: "1234/3")
+    expect(dtc).not_to be_valid
+  end
 end
 
